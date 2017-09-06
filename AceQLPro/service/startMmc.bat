c1a8c188007c7f0befd328544a92c39f67dd3fd0
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2017-09-06 23:28:27 +0200

    Remove files

--- AceQLPro/service/startMmc.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-start /min services.msc
-exit
--- AceQLPro/service/updateServiceUserHomeSystem.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-start /min AceQLHTTPService.exe //US//AceQLHTTPService  ^
- --JvmOptions=-Xrs;-Xms128m;-Xmx256m;-Duser.dir=%1;-Duser.home=%2 ^
-exit
-
-
