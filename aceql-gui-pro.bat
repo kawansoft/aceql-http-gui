e0de68cd929e9089cf99aab5c57df03b4c4badef
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2017-09-06 23:29:35 +0200

    Remove files

--- aceql-gui-pro.bat
+++
@@ -1,2 +0,0 @@
-
-java -Xms128m -Xmx256m -classpath "%CD%\AceQLPro\lib-server\*";"%CD%\AceQLPro\lib-jdbc\*";"%CLASSPATH%" com.kawansoft.aceql.gui.AppTray
--- aceql-gui.bat
+++
@@ -1,2 +0,0 @@
-
-java -Xms128m -Xmx256m -classpath "%CD%\AceQL\lib-server\*";"%CD%\AceQL\lib-jdbc\*";"%CLASSPATH%" com.kawansoft.aceql.gui.AppTray
