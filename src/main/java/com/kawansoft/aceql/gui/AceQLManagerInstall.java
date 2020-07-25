5a931f1ae95597c89c69f0d180bb15eca698255d
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2020-07-25 22:03:11 +0200

    Refactor and follow codacy.com best practices

--- src/main/java/com/kawansoft/aceql/gui/AceQLManagerInstall.java
+++ src/main/java/com/kawansoft/aceql/gui/AceQLManagerInstall.java
@@ -37,7 +37,6 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
