be516cc73224dff8f8817daa4d890bc141da753a
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2020-11-18 15:05:41 +0100

    Version 6.2

--- src/main/java/com/kawansoft/app/version/GuiVersionConstants.java
+++ src/main/java/com/kawansoft/app/version/GuiVersionConstants.java
@@ -28,8 +28,8 @@ package com.kawansoft.app.version;
  */
 
 public class GuiVersionConstants {
-    public static final String VERSION = "v6.1";
-    public static final String DATE = "24-Oct-2020"; 
+    public static final String VERSION = "v6.2";
+    public static final String DATE = "17-Nov-2020"; 
 }
 
 // End
