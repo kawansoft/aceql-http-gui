7d34277e9d6953f02da287ee2b18bf0a5b637abe
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2021-04-19 21:57:09 +0200

    Update AceQLManager form to new L&L

--- src/main/java/com/kawansoft/aceql/gui/AceQLManager.java
+++ src/main/java/com/kawansoft/aceql/gui/AceQLManager.java
@@ -262,7 +262,7 @@ public class AceQLManager extends JFrame {
     }
 
     private void initStart() throws HeadlessException {
-        Dimension dim = new Dimension(610, 610);
+        Dimension dim = new Dimension(615, 615);
         this.setPreferredSize(dim);
         this.setSize(dim);
 
