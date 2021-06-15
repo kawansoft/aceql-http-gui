dbebef804ce3571149b2f4aca84a141a3fb73d99
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2021-06-15 20:17:23 +0200

    Version 7.2 updates

--- src/main/java/com/kawansoft/app/version/GuiVersionConstants.java
+++ src/main/java/com/kawansoft/app/version/GuiVersionConstants.java
@@ -28,8 +28,8 @@ package com.kawansoft.app.version;
  */
 
 public class GuiVersionConstants {
-    public static final String VERSION = "v7.1";
-    public static final String DATE = "07-May-2021"; 
+    public static final String VERSION = "v7.2";
+    public static final String DATE = "15-Jun-2021"; 
 }
 
 // End
