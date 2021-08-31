24e111112099a9ac3077769328c30bcf14b1dc3b
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2021-08-31 16:45:15 +0200

    Update to AceQL HTTP Main 8.0

--- src/main/java/com/kawansoft/app/version/GuiVersionConstants.java
+++ src/main/java/com/kawansoft/app/version/GuiVersionConstants.java
@@ -28,8 +28,8 @@ package com.kawansoft.app.version;
  */
 
 public class GuiVersionConstants {
-    public static final String VERSION = "v7.2";
-    public static final String DATE = "15-Jun-2021"; 
+    public static final String VERSION = "v8.0";
+    public static final String DATE = "31-Aug-2021"; 
 }
 
 // End
