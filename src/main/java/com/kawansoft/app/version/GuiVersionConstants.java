19348024e4983a041b823d4c7b4d6ffd720e8019
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2022-05-27 17:35:26 +0200

    Clean Help

--- src/main/java/com/kawansoft/app/version/GuiVersionConstants.java
+++ src/main/java/com/kawansoft/app/version/GuiVersionConstants.java
@@ -30,7 +30,7 @@ package com.kawansoft.app.version;
 
 public class GuiVersionConstants {
     public static final String VERSION = "v11.0";
-    public static final String DATE = "26-May-2022"; 
+    public static final String DATE = "27-May-2022"; 
 }
 
 // End
