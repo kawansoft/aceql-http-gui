76bfa394a508ab945c005531c025b62f4e9c9e8a
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2022-06-15 17:27:59 +0200

    v11.2 Creation

--- src/main/java/com/kawansoft/app/version/GuiVersionConstants.java
+++ src/main/java/com/kawansoft/app/version/GuiVersionConstants.java
@@ -29,7 +29,7 @@ package com.kawansoft.app.version;
  */
 
 public class GuiVersionConstants {
-    public static final String VERSION = "v11.0";
+    public static final String VERSION = "v11.1";
     public static final String DATE = "15-Jun-2022"; 
 }
 
